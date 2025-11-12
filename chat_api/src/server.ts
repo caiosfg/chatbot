cdacdd24b24234908f34de71102bb60c629399b6
caiosfg <caio.sfg@gmail.com>
2025-11-12 19:08:44 -0300

    feat: create server

---
+++ chat_api/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+class App {
+
+    private app: Application
+    private http: http.Server
+    private io: Server;
+
+    constructor(){
+        this.app = express()
+        this.http = http.createServer(this.app)
+        this.io = new Server(this.http)
+    }
+
+    listenServer() {
+        this.http.listen(3000, () => console.log('server is running on port 3000'))
+    }
+}
+
+const app = new App();
+app.listenServer();
