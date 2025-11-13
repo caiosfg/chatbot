c374fff889641ccc4e43107877d527c86004961f
caiosfg <caio.sfg@gmail.com>
2025-11-13 07:08:05 -0300

    feat: websocket chat

--- chat_api/src/server.ts
+++ chat_api/src/server.ts
@@ -23,6 +23,10 @@ class App {
     listenSocket() {
         this.io.on('connection', (socket) => {
             console.log('user connected =>', socket.id)
+
+            socket.on('message', (msg) => {    
+                this.io.emit('message', msg)
+            })
         })
     }
 
