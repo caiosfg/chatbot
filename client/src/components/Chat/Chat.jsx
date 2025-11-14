27d8a5b6cc7a997969c715eb8d4e7ac37507e988
caiosfg <caio.sfg@gmail.com>
2025-11-14 15:18:10 -0300

    feat: page chat react

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Chat() {
+  return (   
+    <div>
+        <h1>
+            Chat
+        </h1>
+        <input type="text" name="name" id="name" placeholder="Mensagem....." />
+        <button>Enviar</button>
+    </div>
+    )
+}
---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Join() {
+  return (   
+    <div>
+        <h1>
+            Join Chat
+        </h1>
+        <input type="text" name="name" id="name" placeholder="Nome de usuário" />
+        <button>Entrar</button>
+    </div>
+    )
+}
