b374c0735aee816c69dffa5d7bc78c87ff622b7c
caio.sfg <caio.sfg@gmail.com>
2026-05-22 10:49:28 -0300

    feat: Automapper

---
+++ RangoAgil.API/Profiles/RangoAgilProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RangoAgil.API.Entities;
+using RangoAgil.API.Models;
+
+namespace RangoAgil.API.Profiles;
+
+public class RangoAgilProfile : Profile
+{
+    public RangoAgilProfile()
+    {
+        CreateMap<Rango, RangoDTO>().ReverseMap();
+        CreateMap<Ingrediente, IngredienteDTO>()
+            .ForMember(
+                ingredient => ingredient.RangoId,
+                o => o.MapFrom(src => src.Rangos.First().Id)
+            );
+    }
+}
--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -8,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<RangoDbContext>(
     o => o.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConStr"])    
-);         
+);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
